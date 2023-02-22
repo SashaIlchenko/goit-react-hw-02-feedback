cf7fa83be53b61cdd7ea053d427861f7171099cc
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-02-22 18:31:19 +0200

    done 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  handleClickGood = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      }
+    })
+  };
+  handleClickNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      }
+    })
+  };
+  handleClickBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      }
+    })
+  };
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad
+  }
+  countPositiveFeedbackPercentage() {
+    return Math.round(this.state.good * 100 / this.countTotalFeedback())
+  }
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (<div>
+
+      <Section title={"Please leave feedback"}>
+        <div>
+          <button type="button" onClick={this.handleClickGood}>Good</button>
+          <button type="button" onClick={this.handleClickNeutral}>Neutral</button>
+          <button type="button" onClick={this.handleClickBad}>Bad</button></div>
+        {/* <FeedbackOptions options={ } onLeaveFeedback={ } /> */}
+      </Section>
+      <Section title={"Statistics"} >
+        <Statistics good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()} /> </Section>
+    </div>)
+  }
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export const Section = ({ title, children }) => {
+    return (<><h1>{title}</h1> {children}</>)
+}
+console.log(PropTypes)
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+}
